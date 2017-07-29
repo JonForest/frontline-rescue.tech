fd610248f2ffb08709e76901ea603f0e1091d156
MS Deployment Robot <msdeploybot@users.noreply.github.com>
2017-07-30 00:25:59 +1200

    Nats changes
    Updates to styling on all pages now but Checklist and an issue with floating buttons on overview so hidden for now. Will ask Julia/Andrew for help tomorrow.

--- src/app/components/hazards/hazards.component.ts
+++ src/app/components/hazards/hazards.component.ts
@@ -2,17 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hazards',
-  template: `
-    <app-header title="Hazards"></app-header>
-    <div style="min-height:86vh">
-      <h3>Flooding</h3>
-      Possible contiminants in the water.
-    </div>
-    <div>
-      <button class="btn pull-right btn-std">Identify Hazard</button>
-    </div>
-  `,
-  styles: []
+  templateUrl: './hazards.component.html',
+  styleUrls: ['./hazards.component.css']
 })
 export class HazardsComponent implements OnInit {
 
--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -2,19 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
-  template: `
-    <div class="half-screen-map">
-      <app-map (currentAddress)="setAddress($event)"></app-map>
-    </div>
-    <div class="address">
-      {{address}}
-      <button class="btn pull-right" routerLink="/checklist" routerLinkActive="active">Checklist</button>
-    </div>
-  `,
-  styles: [
-    '.half-screen-map { height: 645px}',
-    '.address {padding: 15px;}'
-  ]
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
   public address;

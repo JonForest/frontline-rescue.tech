efa1ec874c5ef32b0711a5ffb09750d1f5d1c0e3
james_brown98 <james@mediasuite.co.nz>
2017-07-30 09:57:20 +1200

    added the checklist page and styling

--- src/app/components/checklist/checklist.component.ts
+++ src/app/components/checklist/checklist.component.ts
@@ -2,18 +2,68 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checklist',
-  template: `
-    <p>
-      checklist Works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './checklist.component.html',
+  styleUrls: ['./checklist.component.css']
 })
 export class ChecklistComponent implements OnInit {
+  hazzards = [
+    {
+      title: 'Flooding',
+      clicked: false,
+    },
+    {
+      title: 'Liquifaction',
+      clicked: false,
+    },
+    {
+      title: 'Exposed cables',
+      clicked: false
+    },
+    {
+      title: 'Cracked ground',
+      clicked: false,
+    },
+    {
+      title: 'Unstable structure',
+      clicked: false,
+    },
+  ];
+
+  assistance = [
+    {
+      title: 'Ambulance',
+      clicked: false,
+    },
+    {
+      title: 'Police',
+      clicked: false,
+    },
+    {
+      title: 'Fire',
+      clicked: false
+    },
+  ];
+  general = [
+    {
+      title: 'No people at the location.',
+    },
+    {
+      title: 'Power was turned off.',
+    },
+    {
+      title: 'Water was turned off.',
+    },
+    {
+      title: 'No pets were seen.',
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleClick(hazzard) {
+    hazzard.clicked = !hazzard.clicked;
+  }
 }
